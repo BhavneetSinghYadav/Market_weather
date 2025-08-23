e51a0344bdc27adb0607458bf41e9847b3c5de18
BhavneetSinghYadav <bhavneet.singh.yadav@gmail.com>
2025-08-23 06:40:46 +0530

    Implement floor_to_minute and add timezone tests

--- mw/utils/time.py
+++ mw/utils/time.py
@@ -1,13 +1,17 @@
-"""
-Time helpers (stub).
-"""
-from datetime import datetime, timezone, timedelta
+"""Time helpers."""
+
+from datetime import datetime, timezone
 
 
 def now_utc() -> datetime:
     """Return the current time as an aware ``datetime`` in UTC."""
     return datetime.now(timezone.utc)
 
+
 def floor_to_minute(dt: datetime) -> datetime:
-    # TODO: implement
-    raise NotImplementedError
+    """Return ``dt`` floored to the start of the minute.
+
+    The original timezone information is preserved.
+    """
+
+    return dt.replace(second=0, microsecond=0)
